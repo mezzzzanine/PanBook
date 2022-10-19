2854c7e2e62be660076fcdb7a3defef5ffa4374b
Almas <akzigitovalmas04@gmail.com>
2022-10-19 20:07:42 +0600

    bot created

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 import telebot
+import config
+from telebot import types
+
+bot = telebot.TeleBot(config.TOKEN)
+
+@bot.message_handler(commands=['start'])
+def answer(message):
+    bot.send_message(message.chat.id, "ПАШОЛ НАХУЙ!")
+
+@bot.message_handler(commands=['info', "bot_info"])
+def answer(message):
+    bot.send_message(message.chat.id, "Че те нада? ИДИ нахуй")
+    
+# code running
+
+bot.polling(none_stop=True)
