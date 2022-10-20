e3f06cfd53178c7d042b92655bcec070f881e65f
Almas <akzigitovalmas04@gmail.com>
2022-10-20 20:03:04 +0600

    New commands added

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
 TOKEN = '5607387053:AAEfVeo5MmtDzzduOgQJYNJqiEBy_QYzUtI'
+
+INFO = """Hello, I'm PanBook Bot. With my help, you can find a book to your liking.
+
+Use these commands to control me:
+
+/charts - Get a list of the most popular books of this week
+/search - Find book
+/help - Get help"""
--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import telebot
 import config
-from telebot import types
+# from telebot import types
 
 bot = telebot.TeleBot(config.TOKEN)
 
-@bot.message_handler(commands=['start'])
-def answer(message):
-    bot.send_message(message.chat.id, "ПАШОЛ НАХУЙ!")
 
-@bot.message_handler(commands=['info', "bot_info"])
-def answer(message):
-    bot.send_message(message.chat.id, "Че те нада? ИДИ нахуй")
-    
+@bot.message_handler(commands=['start', 'help'])  # decorator pattern are used
+def start(message):
+    bot.send_message(message.chat.id, config.INFO)
+
+
+@bot.message_handler(commands=['charts'])  # decorator pattern are used
+def start(message):
+    bot.send_message(message.chat.id, 'Most popular books of this week:')
+
+
+@bot.message_handler(commands=['search'])  # decorator pattern are used
+def start(message):
+    bot.send_message(message.chat.id, 'Please type')
+
+
 # code running
 
 bot.polling(none_stop=True)
